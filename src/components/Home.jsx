77b7e2f926792fd2c6a5b1dba986e22ff29f5b90
Diego Faria <diegoafbrito12@gmail.com>
2022-11-05 14:05:27 -0400

    añadir home de gerente

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import download from 'downloadjs';
 
+import './css/gerente.css';
+
+import img1 from './Img/dde6daf4675eec6dc7e0c73a83e37bd2_1.png';
+import img2 from './Img/_8a36117830713c529bb94c295c5255b5_1.png';
+import img3 from './Img/mask_group_ek1.png';
+import img4 from './Img/logo_ek1.png';
+import img5 from './Img/mask_group_ek2.png';
+
 const Home = () => {
 
   const [datos, setDatos] = useState({});
@@ -99,48 +107,113 @@ const Home = () => {
     return (<h3>Cargando...</h3>)
 
   return (
-    <div>
-        <h1>¡Bienvenido, {datos.empleado.cargo} {datos.persona.nombre}!</h1>
-        <h5 style={{textAlign: "left"}}>Creación</h5>
-        <ol style={{textAlign: "left"}}>
-          <li><Link to="/crear/clientes">Crear Cliente</Link></li>
-          <li><Link to="/crear/empleados">Crear Empleado</Link></li>
-          <li><Link to="/crear/maquinas">Crear Máquinas</Link></li>
-          <li><Link to="/crear/transaccion">Crear Transacción</Link></li>
-        </ol>
+    <main className="main-container">
+		<input className='input-gerente' type="checkbox" name="" id="check" />
+		<div class="div-gerente menu">
+			<label for="check">
+				<span class="span-gerente fas fa-times" id="times"></span>
+				<span class="span-gerente fas fa-circle-user" id="bars"></span>
+			</label>
+			<div class="div-gerente head"> menu </div>
 
-        <h5 style={{textAlign: "left"}}>Modificación</h5>
-        <ol style={{textAlign: "left"}}>
-          <li><Link to="/modificar/clientes">Modificar Cliente</Link></li>
-          <li><Link to="/modificar/empleados">Modificar Empleado</Link></li>
-          <li><Link to="/modificar/maquinas">Modificar Máquinas</Link></li>
-        </ol>
+			<li><a href="#"><i class="fas fa-users"></i> Manual de ayuda de usuario</a></li>
+			<li><a href="#"><i class="fas fa-cloud"></i> Respaldo de base de datos</a></li>
+			<li><a href="#"><i class="fas fa-gear"></i> Configuracion</a></li>
+			<li><a href="#"><i class="fas fa-info"></i> Acerca de</a></li>
+			<li><a href="#"><i class="fas fa-gamepad"></i> Probar maquina</a></li>
 
-        <h5 style={{textAlign: "left"}}>Consultas</h5>
-        <ol style={{textAlign: "left"}}>
-          <li><Link to="/consultar/clientes">Consultar Clientes</Link></li>
-          <li><Link to="/consultar/empleados">Consultar Empleados</Link></li>
-          <li><Link to="/consultar/tarjeta">Consultar Tarjeta</Link></li>
-          <li><Link to="/consultar/maquinas">Consultar Máquina</Link></li>
-        </ol>
 
-        <h5 style={{textAlign: "left"}}>Reportes</h5>
-        <ol style={{textAlign: "left"}}>
-          <li><a href='#1' onClick={reporteClientes}>Reporte de Clientes</a></li>
-          <li><a href='#2' onClick={reporteEmpleados}>Reporte de Empleados</a></li>
-          <li><a href='#3' onClick={reporteMaquinas}>Reporte de Máquinas</a></li>
-        </ol> 
+		</div>
+		<img src={img1} class="img-1" />
+		<img src={img2} class="img-2" />
+		<img src={img3} class="img-3" />
 
-        <h5 style={{textAlign: "left"}}>Otros</h5>
-        <ol style={{textAlign: "left"}}>
-          <li><Link to="/control/probar_maquina">Probar Máquina</Link></li>
-          <li><a href='#4' onClick={crearBackup}>Hacer Respaldo de Base de Datos</a></li>
-          <li><a href='#5' onClick={restaurarBackup}>Restaurar Último Respaldo de la Base de Datos</a></li>
-          <li><Link to="/control/cambiar_contrasena">Cambiar Contraseña</Link></li>
-        </ol> 
+		<div class="div-gerente barra_de_navegacion">
+			<div class="div-gerente reportes">
+				<button type="button" class="btn btn-white dropdownd-toggle" id="reportes" data-toggle="dropdown"
+					aria-haspopup="true" aria-expanded="true">
+					Reportes
+				</button>
+				<div class="div-gerente dropdown-menu">
+					<li><a href="#" class="dropdown-item">Reporte de Empleado</a></li>
+					<li><a href="#" class="dropdown-item">Reporde de Cliente</a></li>
+					<li><a href="#" class="dropdown-item">Reporte de Maquina</a></li>
 
-        <button onClick={() => {logOut();}}>Salir</button>
-    </div>
+
+				</div>
+
+			</div>
+
+			<div class="div-gerente agregar">
+
+				<button type="button" class="btn btn-white dropdownd-toggle" id="agregar" data-toggle="dropdown"
+					aria-haspopup="true" aria-expanded="true">
+					Agregar
+				</button>
+				<div class="div-gerente dropdown-menu">
+					<li><a href="#" class="dropdown-item">Agregar Cliente</a></li>
+					<li><a href="#" class="dropdown-item">Agregar Empleado</a></li>
+					<li><a href="#" class="dropdown-item">Agregar Maquina</a></li>
+					<li><a href="#" class="dropdown-item">Agregar Transacción</a></li>
+
+				</div>
+			</div>
+			<div class="div-gerente consultar">
+				<button type="button" class="btn btn-white dropdownd-toggle" id="consultar" data-toggle="dropdown"
+					aria-haspopup="true" aria-expanded="true">
+					Consultar
+				</button>
+				<div class="div-gerente dropdown-menu">
+					<li><a href="#" class="dropdown-item">Consultar Cliente</a></li>
+					<li><a href="#" class="dropdown-item">Consultar Empleado</a></li>
+					<li><a href="#" class="dropdown-item">Consultar Tarjeta</a></li>
+					<li><a href="#" class="dropdown-item">Consultar Maquina</a></li>
+
+
+				</div>
+			</div>
+			<div class="div-gerente modificar">
+				<button type="button" class="btn btn-white dropdownd-toggle" id="modificar" data-toggle="dropdown"
+					aria-haspopup="true" aria-expanded="true">
+					Modificar
+				</button>
+				<div class="div-gerente dropdown-menu">
+					<li><a href="#" class="dropdown-item">Modificar Cliente</a></li>
+					<li><a href="#" class="dropdown-item">Modificar Empleado</a></li>
+					<li><a href="#" class="dropdown-item">Modificar Maquina</a></li>
+
+				</div>
+			</div>
+
+
+			<img src={img4} class="logo" />
+
+		</div>
+
+		<div class="div-gerente outer button">
+
+			<button onClick={logOut}> LOG OUT </button>
+			<span className='span-gerente'></span>
+			<span className='span-gerente'></span>
+		</div>
+
+		<div className='div-gerente' id="nombre_del_gerente">
+			{datos.persona.nombre}
+		</div>
+		<div className='div-gerente' id="__0000">
+			#{datos.persona.cedula}
+		</div>
+		<div className='div-gerente' id="bienvenido">
+			¡BIENVENIDO ESTIMADO GERENTE!
+		</div>
+		<div className='div-gerente' id="saludo">
+			Aquí encontrarás las funciones administrativas de ARCADESTATION. Echa un
+			vistazo y descubre sus funciones, o retoma las actividades que has dejado inconclusas.
+		</div>
+
+		<img src={img5} id="mask_group_ek2" />
+
+	</main>
   )
 }
 
