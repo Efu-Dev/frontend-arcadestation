61e47a80113120087dde59c1bfb855a0664f2d30
Diego Faria <diegoafbrito12@gmail.com>
2022-11-05 14:45:03 -0400

    añadidas bienvenidas cajero y gerente

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import download from 'downloadjs';
 
-import './css/gerente.css';
+import './css/home.css';
 
 import img1 from './Img/dde6daf4675eec6dc7e0c73a83e37bd2_1.png';
 import img2 from './Img/_8a36117830713c529bb94c295c5255b5_1.png';
 import img3 from './Img/mask_group_ek1.png';
 import img4 from './Img/logo_ek1.png';
 import img5 from './Img/mask_group_ek2.png';
+
+import img6 from './Img/dde6daf4675eec6dc7e0c73a83e37bd2_1_ek2.png';
+import img7 from './Img/_238738_1.png';
 
 const Home = () => {
 
@@ -106,115 +109,182 @@ const Home = () => {
   if(loading)
     return (<h3>Cargando...</h3>)
 
-  return (
-    <main className="main-container">
-		<input className='input-gerente' type="checkbox" name="" id="check" />
-		<div class="div-gerente menu">
-			<label for="check">
-				<span class="span-gerente fas fa-times" id="times"></span>
-				<span class="span-gerente fas fa-circle-user" id="bars"></span>
-			</label>
-			<div class="div-gerente head"> menu </div>
+  if(datos.empleado.cargo === 'Gerente')
+    return (
+      <main className="main-container">
+      <input className='input-gerente' type="checkbox" name="" id="check" />
+      <div class="div-gerente menu">
+        <label for="check">
+          <span class="span-gerente fas fa-times" id="times"></span>
+          <span class="span-gerente fas fa-circle-user" id="bars"></span>
+        </label>
+        <div class="div-gerente head" style={{marginBottom: '50px'}}> menu </div>
 
-			<li><a href="#"><i class="fas fa-users"></i> Manual de ayuda de usuario</a></li>
-			<li><a href="#"><i class="fas fa-cloud"></i> Respaldo de base de datos</a></li>
-			<li><a href="#"><i class="fas fa-gear"></i> Configuracion</a></li>
-			<li><a href="#"><i class="fas fa-info"></i> Acerca de</a></li>
-			<li><a href="#"><i class="fas fa-gamepad"></i> Probar maquina</a></li>
-
-
-		</div>
-		<img src={img1} class="img-1" />
-		<img src={img2} class="img-2" />
-		<img src={img3} class="img-3" />
-
-		<div class="div-gerente barra_de_navegacion">
-			<div class="div-gerente reportes">
-				<button type="button" class="btn btn-white dropdownd-toggle" id="reportes" data-toggle="dropdown"
-					aria-haspopup="true" aria-expanded="true">
-					Reportes
-				</button>
-				<div class="div-gerente dropdown-menu">
-					<li><a href="#" class="dropdown-item">Reporte de Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Reporde de Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Reporte de Maquina</a></li>
+        
+          <li><a href="#"><i class="fas fa-users"></i> Manual de usuario</a></li>
+          <li><a href="#" onClick={crearBackup}><i class="fas fa-cloud"></i> Crear Respaldo de base de datos</a></li>
+          <li><a href="#" onClick={restaurarBackup}><i class="fas fa-cloud"></i> Restaurar base de datos</a></li>
+          <li><a href="/control/cambiar_contrasena"><i class="fas fa-gear"></i> Cambiar Contraseña</a></li>
+          <li><a href="/control/probar_maquina"><i class="fas fa-gamepad"></i> Probar máquina</a></li>
 
 
-				</div>
+      </div>
+      <img src={img1} class="img-1" />
+      <img src={img2} class="img-2" />
+      <img src={img3} class="img-3" />
 
-			</div>
+      <div class="div-gerente barra_de_navegacion">
+        <div class="div-gerente reportes">
+          <button type="button" class="btn btn-white dropdownd-toggle" id="reportes" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="true">
+            Reportes
+          </button>
+          <div class="div-gerente dropdown-menu">
+            <li><a href="#reporte_empleado" onClick={reporteEmpleados} class="dropdown-item">Reporte de Empleados</a></li>
+            <li><a href="#reporte_cliente" onClick={reporteClientes} class="dropdown-item">Reporte de Clientes</a></li>
+            <li><a href="#reporte_maquina" onClick={reporteMaquinas} class="dropdown-item">Reporte de Máquinas</a></li>
+          </div>
 
-			<div class="div-gerente agregar">
+        </div>
 
-				<button type="button" class="btn btn-white dropdownd-toggle" id="agregar" data-toggle="dropdown"
-					aria-haspopup="true" aria-expanded="true">
-					Agregar
-				</button>
-				<div class="div-gerente dropdown-menu">
-					<li><a href="#" class="dropdown-item">Agregar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Maquina</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Transacción</a></li>
+        <div class="div-gerente agregar">
+          <button type="button" class="btn btn-white dropdownd-toggle" id="agregar" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="true">
+            Agregar
+          </button>
+          <div class="div-gerente dropdown-menu">
+            <li><a href="/crear/clientes" class="dropdown-item">Agregar Cliente</a></li>
+            <li><a href="/crear/empleados" class="dropdown-item">Agregar Empleado</a></li>
+            <li><a href="/crear/maquinas" class="dropdown-item">Agregar Máquina</a></li>
+            <li><a href="/crear/transaccion" class="dropdown-item">Agregar Recarga</a></li>
+          </div>
+        </div>
+        <div class="div-gerente consultar">
+          <button type="button" class="btn btn-white dropdownd-toggle" id="consultar" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="true">
+            Consultar
+          </button>
+          <div class="div-gerente dropdown-menu">
+            <li><a href="/consultar/clientes" class="dropdown-item">Consultar Cliente</a></li>
+            <li><a href="/consultar/empleados" class="dropdown-item">Consultar Empleado</a></li>
+            <li><a href="/consultar/tarjeta" class="dropdown-item">Consultar Tarjeta</a></li>
+            <li><a href="/consultar/maquinas" class="dropdown-item">Consultar Maquina</a></li>
+          </div>
+        </div>
+        <div class="div-gerente modificar">
+          <button type="button" class="btn btn-white dropdownd-toggle" id="modificar" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="true">
+            Modificar
+          </button>
+          <div class="div-gerente dropdown-menu">
+            <li><a href="/modificar/clientes" class="dropdown-item">Modificar Cliente</a></li>
+            <li><a href="/modificar/empleados" class="dropdown-item">Modificar Empleado</a></li>
+            <li><a href="/modificar/maquinas" class="dropdown-item">Modificar Maquina</a></li>
 
-				</div>
-			</div>
-			<div class="div-gerente consultar">
-				<button type="button" class="btn btn-white dropdownd-toggle" id="consultar" data-toggle="dropdown"
-					aria-haspopup="true" aria-expanded="true">
-					Consultar
-				</button>
-				<div class="div-gerente dropdown-menu">
-					<li><a href="#" class="dropdown-item">Consultar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Tarjeta</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Maquina</a></li>
-
-
-				</div>
-			</div>
-			<div class="div-gerente modificar">
-				<button type="button" class="btn btn-white dropdownd-toggle" id="modificar" data-toggle="dropdown"
-					aria-haspopup="true" aria-expanded="true">
-					Modificar
-				</button>
-				<div class="div-gerente dropdown-menu">
-					<li><a href="#" class="dropdown-item">Modificar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Modificar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Modificar Maquina</a></li>
-
-				</div>
-			</div>
+          </div>
+        </div>
 
 
-			<img src={img4} class="logo" />
+        <img src={img4} class="logo" />
 
-		</div>
+      </div>
 
-		<div class="div-gerente outer button">
+      <div class="div-gerente outer button">
 
-			<button onClick={logOut}> LOG OUT </button>
-			<span className='span-gerente'></span>
-			<span className='span-gerente'></span>
-		</div>
+        <button onClick={logOut}> LOG OUT </button>
+        <span className='span-gerente'></span>
+        <span className='span-gerente'></span>
+      </div>
 
-		<div className='div-gerente' id="nombre_del_gerente">
-			{datos.persona.nombre}
-		</div>
-		<div className='div-gerente' id="__0000">
-			#{datos.persona.cedula}
-		</div>
-		<div className='div-gerente' id="bienvenido">
-			¡BIENVENIDO ESTIMADO GERENTE!
-		</div>
-		<div className='div-gerente' id="saludo">
-			Aquí encontrarás las funciones administrativas de ARCADESTATION. Echa un
-			vistazo y descubre sus funciones, o retoma las actividades que has dejado inconclusas.
-		</div>
+      <div className='div-gerente' id="nombre_del_gerente">
+        {datos.persona.nombre}
+      </div>
+      <div className='div-gerente' id="__0000">
+        #{datos.persona.cedula}
+      </div>
+      <div className='div-gerente' id="bienvenido">
+        ¡BIENVENIDO ESTIMADO GERENTE!
+      </div>
+      <div className='div-gerente' id="saludo">
+        Aquí encontrarás las funciones administrativas de ARCADESTATION. Echa un
+        vistazo y descubre sus funciones, o retoma las actividades que has dejado inconclusas.
+      </div>
 
-		<img src={img5} id="mask_group_ek2" />
+      <img src={img5} id="mask_group_ek2" />
+      </main>
+   )
 
+  else 
+      return(
+        <main class="main-container">
+
+        <input className='input-gerente' type="checkbox" name="" id="check" />
+        <div class="div-gerente menu">
+          <label for="check">
+            <span class="fas fa-times" id="times"></span>
+            a
+            <span class="far fa-circle-user" id="bars"></span>
+          </label>
+        <div class="div-gerente head"> menu </div> <br /> <br /> <br /> <br />
+          <li><a href="#"><i class="fas fa-users"></i> Manual de ayuda de usuario</a></li>
+          <li><a href="/control/cambiar_contrasena"><i class="fas fa-gear"></i> Cambiar Contraseña</a></li>
+          <li><a href="/control/probar_maquina"><i class="fas fa-info"></i> Probar Máquina</a></li>
+        </div>
+
+        <img src={img6} class="img-1" />
+        <img src={img7} class="img-2" />
+        <img src={img3} class="img-3" />
+
+        <div class="div-gerente barra_de_navegacion">
+          <div class="div-gerente crear">
+            <button type="button" class="btn btn-white dropdown-toggle" id="Crear" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="true">
+              Crear
+            </button>
+            <div class="div-gerente dropdown-menu">
+              <li><a href="/crear/clientes" class="dropdown-item">Crear Cliente</a></li>
+              <li><a href="/crear/transaccion" class="dropdown-item">Crear Recarga</a></li>
+            </div>
+          </div>
+
+          <div class="div-gerente consultar">
+            <button type="button" class="btn btn-white dropdown-toggle" id="Consultar" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="true">
+              Consultar
+            </button>
+            <div class="div-gerente dropdown-menu">
+              <li><a href="/consultar/tarjeta" class="dropdown-item">Consultar tarjeta</a></li>
+              
+            </div>
+          </div>
+
+          <img src={img4} class="logo" />
+        </div>
+
+        <div class="div-gerente outer button">
+
+          <button onClick={logOut}> LOG OUT </button>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className='div-gerente' id="nombre_del_gerente">
+          {datos.persona.nombre}
+        </div>
+        <div className='div-gerente' id="__0000">
+          #{datos.persona.cedula}
+        </div>
+        <div className='div-gerente' id="bienvenido">
+          ¡BIENVENIDO ESTIMADO CAJERO!
+        </div>
+        <div className='div-gerente' id="saludo">
+          Aquí tendrás tus funciones laborales de ARCADESTATION. Echa un vistazo y descubre tus funciones, en caso
+          de una duda utilice el boton de ayuda.
+        </div>
+
+        <img src={img5} id="mask_group_ek2" />
 	</main>
-  )
+      )
 }
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,97 +3,150 @@ import img1 from './Img/dde6daf4675eec6dc7e0c73a83e37bd2_1.png';
 import img2 from './Img/_8a36117830713c529bb94c295c5255b5_1.png';
 import img3 from './Img/mask_group_ek1.png'
 import img4 from './Img/logo_ek1.png';
-import './css/gerente.module.css';
+
+import axios from 'axios';
+import download from 'downloadjs';
 
 const Navbar = ({usuario}) => {
+
+	const reporteClientes = () => {
+		axios.create({
+		  baseURL: 'https://arcadestation.pythonanywhere.com/reportes/generar/clientes',
+		  'headers': {
+			'Authorization': localStorage.getItem('access_token_as')
+		  }
+		}).get().then((res) => {
+		  const content = res.headers['content-type'];
+		  download(res.data, 'REPORTE_CLIENTES_ARCADESTATION.pdf', content)
+		}).catch((e) => console.log(e));
+	  };
+	
+	  const reporteEmpleados = () => {
+		axios.create({
+		  baseURL: 'https://arcadestation.pythonanywhere.com/reportes/generar/empleados',
+		  'headers': {
+			'Authorization': localStorage.getItem('access_token_as')
+		  }
+		}).get().then((res) => {
+		  const content = res.headers['content-type'];
+		  download(res.data, 'REPORTE_EMPLEADOS_ARCADESTATION.pdf', content)
+		}).catch((e) => console.log(e));
+	  };
+	
+	  const reporteMaquinas = () => {
+		axios.create({
+		  baseURL: 'https://arcadestation.pythonanywhere.com/reportes/generar/máquinas',
+		  'headers': {
+			'Authorization': localStorage.getItem('access_token_as')
+		  }
+		}).get().then((res) => {
+		  const content = res.headers['content-type'];
+		  download(res.data, 'REPORTE_MAQUINAS_ARCADESTATION.pdf', content)
+		}).catch((e) => console.log(e));
+	  };
+	
+	  const crearBackup = () => {
+		axios.create({
+		  baseURL: 'https://arcadestation.pythonanywhere.com/backupdb/',
+		  'headers': {
+			'Authorization': localStorage.getItem('access_token_as')
+		  }
+		}).get().then((res) => {
+		  if(res.data.message === 'Success')
+			alert('El respaldo ha sido generado y enviado a la nube MEGA de la empresa.');
+		  else
+			alert('No se pudo crear el respaldo.');
+		}).catch((e) => console.log(e));
+	  };
+	
+	  const restaurarBackup = () => {
+		axios.create({
+		  baseURL: 'https://arcadestation.pythonanywhere.com/backupdb/',
+		  'headers': {
+			'Authorization': localStorage.getItem('access_token_as')
+		  }
+		}).post().then((res) => {
+		  if(res.data.message === 'Success')
+			alert('El último respaldo guardado en la nube de la empresa ha sido restaurado exitosamente.');
+		  else
+			alert('El último respaldo guardado en la nube de la empresa NO pudo ser restaurado.');
+		}).catch((e) => console.log(e));
+	  };
     
   return (
-    <div class="menu">
-		<label for="check">
-			<span class="fas fa-times" id="times"></span>
-			<span class="far fa-circle-user" id="bars"></span>
-		</label>
-		<div class="head"> menu 
+    <div class="div-gerente menu">
+			<label for="check">
+				<span class="span-gerente fas fa-times" id="times"></span>
+				<span class="span-gerente fas fa-circle-user" id="bars"></span>
+			</label>
+			<div class="div-gerente head" style={{marginBottom: '50px'}}> menu </div>
 
-			<li><a href="#"><i class="fas fa-users"></i> Manual de ayuda de usuario</a></li>
-			<li><a href="#"><i class="fas fa-cloud"></i> Respaldo de base de datos</a></li>
-			<li><a href="#"><i class="fas fa-gear"></i> Configuracion</a></li>
-			<li><a href="#"><i class="fas fa-info"></i> Acerca de</a></li>
-			<li><a href="#"><i class="fas fa-gamepad"></i> Probar maquina</a></li>
+      
+        <li><a href="#"><i class="fas fa-users"></i> Manual de usuario</a></li>
+        <li><a href="#"><i class="fas fa-cloud"></i> Crear Respaldo de base de datos</a></li>
+        <li><a href="#"><i class="fas fa-cloud"></i> Restaurar base de datos</a></li>
+        <li><a href="#"><i class="fas fa-gear"></i> Cambiar Contraseña</a></li>
+        <li><a href="#"><i class="fas fa-gamepad"></i> Probar máquina</a></li>
 
-		</div>
 		<img src={img1} class="img-1" />
 		<img src={img2} class="img-2" />
 		<img src={img3} class="img-3" />
 
-		<div class="barra_de_navegacion">
-			<div class="reportes">
+		<div class="div-gerente barra_de_navegacion">
+			<div class="div-gerente reportes">
 				<button type="button" class="btn btn-white dropdownd-toggle" id="reportes" data-toggle="dropdown"
 					aria-haspopup="true" aria-expanded="true">
 					Reportes
 				</button>
-				<div class="dropdown-menu">
-					<li><a href="#" class="dropdown-item">Reporte de Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Reporde de Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Reporte de Maquina</a></li>
-
-
+				<div class="div-gerente dropdown-menu">
+					<li><a href="#reporte_empleado" onClick={reporteEmpleados} class="dropdown-item">Reporte de Empleados</a></li>
+					<li><a href="#reporte_cliente" onClick={reporteClientes} class="dropdown-item">Reporte de Clientes</a></li>
+					<li><a href="#reporte_maquina" onClick={reporteMaquinas} class="dropdown-item">Reporte de Máquinas</a></li>
 				</div>
 
 			</div>
 
-			<div class="agregar">
-
+			<div class="div-gerente agregar">
 				<button type="button" class="btn btn-white dropdownd-toggle" id="agregar" data-toggle="dropdown"
 					aria-haspopup="true" aria-expanded="true">
 					Agregar
 				</button>
-				<div class="dropdown-menu">
-					<li><a href="#" class="dropdown-item">Agregar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Maquina</a></li>
-					<li><a href="#" class="dropdown-item">Agregar Transacción</a></li>
-
+				<div class="div-gerente dropdown-menu">
+					<li><a href="/crear/clientes" class="dropdown-item">Agregar Cliente</a></li>
+					<li><a href="/crear/empleados" class="dropdown-item">Agregar Empleado</a></li>
+					<li><a href="/crear/maquinas" class="dropdown-item">Agregar Máquina</a></li>
+					<li><a href="/crear/transaccion" class="dropdown-item">Agregar Recarga</a></li>
 				</div>
 			</div>
-			<div class="consultar">
+			<div class="div-gerente consultar">
 				<button type="button" class="btn btn-white dropdownd-toggle" id="consultar" data-toggle="dropdown"
 					aria-haspopup="true" aria-expanded="true">
 					Consultar
 				</button>
-				<div class="dropdown-menu">
-					<li><a href="#" class="dropdown-item">Consultar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Tarjeta</a></li>
-					<li><a href="#" class="dropdown-item">Consultar Maquina</a></li>
-
-
+				<div class="div-gerente dropdown-menu">
+					<li><a href="/consultar/clientes" class="dropdown-item">Consultar Cliente</a></li>
+					<li><a href="/consultar/empleados" class="dropdown-item">Consultar Empleado</a></li>
+					<li><a href="/consultar/tarjeta" class="dropdown-item">Consultar Tarjeta</a></li>
+					<li><a href="/consultar/maquinas" class="dropdown-item">Consultar Maquina</a></li>
 				</div>
 			</div>
-			<div class="modificar">
+			<div class="div-gerente modificar">
 				<button type="button" class="btn btn-white dropdownd-toggle" id="modificar" data-toggle="dropdown"
 					aria-haspopup="true" aria-expanded="true">
 					Modificar
 				</button>
-				<div class="dropdown-menu">
-					<li><a href="#" class="dropdown-item">Modificar Cliente</a></li>
-					<li><a href="#" class="dropdown-item">Modificar Empleado</a></li>
-					<li><a href="#" class="dropdown-item">Modificar Maquina</a></li>
+				<div class="div-gerente dropdown-menu">
+					<li><a href="/modificar/clientes" class="dropdown-item">Modificar Cliente</a></li>
+					<li><a href="/modificar/empleados" class="dropdown-item">Modificar Empleado</a></li>
+					<li><a href="/modificar/maquinas" class="dropdown-item">Modificar Maquina</a></li>
+
 				</div>
 			</div>
 
+
 			<img src={img4} class="logo" />
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-				integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-				crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-				integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-				crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-				integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-				crossorigin="anonymous"></script>
+			</div>
 		</div>
-    </div>
   );
 };
 
